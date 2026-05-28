5dcf2f1f8d29e6ee3f3306525316948c660867d0
Ritam Das <ritam.das3110@outlook.com>
2026-05-28 14:11:07 +0000

    undo params preservation

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,11 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
-export const load = ({ url }) => {
-	const target = new URL('https://ccuhacks.devfolio.co/');
-
-	for (const [key, value] of url.searchParams.entries()) {
-		target.searchParams.set(key, value);
-	}
-
-	redirect(302, target.toString());
+export const load = () => {
+	redirect(302, 'https://ccuhacks.devfolio.co/');
 };
