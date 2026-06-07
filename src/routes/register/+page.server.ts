d98ab946fcba065181e097a5a713f11873657816
Ritam Das <86097735+nxrmqlly@users.noreply.github.com>
2026-06-07 17:20:21 +0530

    Update /register redirect

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from '@sveltejs/kit';
 
 export const load = () => {
-	redirect(302, 'https://ccuhacks.devfolio.co/');
+	redirect(302, 'https://ccuhacks.fillout.com/register');
 };
